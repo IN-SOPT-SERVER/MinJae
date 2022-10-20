673e0e9318aa0c93be6b2ed129db9468ce50f9bc
Minjae Kang <rkdalswo1021@naver.com>
2022-10-20 17:21:05 +0900

    feat: 3차 세미나 발전 과제

--- seminar3-netflix/src/router/index.ts
+++ seminar3-netflix/src/router/index.ts
@@ -1,5 +1,8 @@
 import express, { Router } from "express";
+import movieRouter from "./movieRouter";
 
 const router: Router = express.Router();
+
+router.use("/movies", movieRouter);
 
 export default router;
--- seminar3-netflix/src/service/MovieService.ts
+++ seminar3-netflix/src/service/MovieService.ts
@@ -1,3 +1,10 @@
 const data = require("../database/data.json");
 
-export default {};
+const getMovie = async (movieId: string, episode: string) => {
+  if (!episode) {
+    return data[movieId];
+  }
+  return data[movieId][episode];
+};
+
+export default { getMovie };
